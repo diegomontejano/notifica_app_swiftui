e5ff665a78314bc5b254229c7b1f0493c18afc1f
Diego <diegomontejano@icloud.com>
2022-05-23 14:10:44 -0300

    setting countdown logic

--- notifica/Components/CountdownBar.swift
+++ notifica/Components/CountdownBar.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct CountdownBar: View {
     let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
+    @State private var progressBar: Double = 0.0
     @EnvironmentObject var appController: AppController
 
 var body: some View {
@@ -13,15 +14,16 @@ var body: some View {
                     .foregroundColor(.grayColor2)
                     .cornerRadius(10)
                 Rectangle()
-                    .frame(width: geo.size.width*appController.countdownTimer, height: 8)
+                    .frame(width: geo.size.width*progressBar, height: 8)
                     .foregroundColor(.orange)
                     .cornerRadius(10)
                     .onReceive(timer) { _ in // start animation
-                        if appController.countdownTimer < 1.0 {
-                            appController.countdownTimer += 0.01
+                        if appController.countdownTimer > 0.0 {
+                            appController.countdownTimer -= 1.0
+                            progressBar += 0.01
                         } else {
                             appController.showNotificationBanner = true
-                            appController.countdownTimer = 0.0
+                            appController.countdownTimer = 100.0
                         }
                     }
                 }
