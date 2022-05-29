9a31b8c0b819c869f75a4c652b68a0744fd48502
Diego <diegomontejano@icloud.com>
2022-05-29 00:42:34 -0300

    implementing real notifications

--- notifica/ViewModels/AppController.swift
+++ notifica/ViewModels/AppController.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftUI
+import UserNotifications
 
 class AppController: ObservableObject {
     @Published var showNotificationBanner: Bool = false
@@ -19,7 +20,7 @@ class AppController: ObservableObject {
         return (Double(counter) / Double(cargaHorariaDiaria))
     }
     
-    // runned on load MainView()
+    // runned on load HomeView()
     func simulateTimer() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
             if self.counter < self.cargaHorariaDiaria {
@@ -35,7 +36,32 @@ class AppController: ObservableObject {
     func simulateNotificationBanner() {
         if self.enableNotificationBanner == true && counterToMinutes.contains("0h e \(String(notificarQuandoFaltar))min")
             || self.enableNotificationBanner == true && counterToMinutes == "0h e 05min" {
-            self.showNotificationBanner = true
+             self.showNotificationBanner = true
+            self.realNotificationBanner()
         }
+    }
+    
+    func realNotificationBanner() {
+        // request user authorization for app notifications
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {success, error in
+            if success {
+                print("Notification allowed")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "UI Conecta"
+        notificationContent.subtitle = "Seu horário terminará em \(String(notificarQuandoFaltar)) minutos"
+        notificationContent.body = "Evite telemetria, prepare-se para passar o ponto"
+        notificationContent.badge = 1
+        notificationContent.sound = UNNotificationSound.default
+
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.5, repeats: false)
+        
+        let notificationRequest = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: notificationTrigger)
+
+        UNUserNotificationCenter.current().add(notificationRequest)
     }
 }
